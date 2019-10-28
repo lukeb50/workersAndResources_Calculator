b6fd37b499bbfc33e593678d0b0e9f188de34cbb
lukeb50 <31168500+lukeb50@users.noreply.github.com>
2019-10-28 14:34:41 -0400

    Update index.js

--- Process_Image/index.js
+++ Process_Image/index.js
@@ -6,6 +6,12 @@ const bucket = storage.bucket('report-cards-6290-uploads');
 const vision = require('@google-cloud/vision');
 const client = new vision.ImageAnnotatorClient();
 
+async function getText() {
+    const [result] = await client.documentTextDetection("gs://report-cards-6290-uploads/" + req.body.loc);
+    const fullTextAnnotation = result.fullTextAnnotation;
+    console.log(fullTextAnnotation.text);
+}
+
 exports.Process = (req, res) => {
     res.set('Access-Control-Allow-Origin', "*");
     res.set('Access-Control-Allow-Methods', 'POST');
@@ -13,8 +19,8 @@ exports.Process = (req, res) => {
         res.status(400).end;
     }
     client.documentTextDetection("gs://report-cards-6290-uploads/" + req.body.loc).then(response => {
-        for (var i = 0; i < response[0].textAnnotations.length; i++) {
-            console.log(response[0].textAnnotations[i]);
+        for (var i = 0; i < response[0].fullTextAnnotations.length; i++) {
+            console.log(response[0].fullTextAnnotations[i]);
         }
         res.status(201).end();
     }).catch(err => {
