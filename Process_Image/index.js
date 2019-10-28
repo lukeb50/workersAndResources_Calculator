8e1fa7b61c4aa4b8bafa0afc3ccf7faa5e5a4dbe
lukeb50 <31168500+lukeb50@users.noreply.github.com>
2019-10-28 11:22:42 -0400

    Update index.js

--- Process_Image/index.js
+++ Process_Image/index.js
@@ -13,10 +13,10 @@ exports.Process = (req, res) => {
         res.status(400).end;
     }
     client.documentTextDetection("gs://report-cards-6290-uploads/"+req.body.loc).then(response => {
-        response.labelAnnotations.forEach(label => console.log(label.description));
+        (response.labelAnnotations).forEach(label => console.log(label.description));
         res.status(201).end();
     }).catch (err=>{
         console.log("error:"+err.toString());
         res.status(500).end();
     });
-}
+};
